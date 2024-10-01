034489bbff4b93952c856740d961ee7acaae0372
Jamie Dovaston <jamiedovaston@outlook.com>
2024-10-01 02:00:10 +0100

    Created Character Controller Week 0
    Wrote the code for the Character Controller however had problems linking rider and unreal.

---
+++ Source/Project/Private/FirstActor.cpp
@@ -0,0 +1,11 @@
+﻿#include "FirstActor.h"
+
+AFirstActor::AFirstActor()
+{
+	_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = _Root;
+
+	_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	_Mesh->SetupAttachment(_Root);
+}
+
---
+++ Source/Project/Private/FirstActor.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FirstActor.generated.h"
+
+UCLASS(Abstract)
+class PROJECT_API AFirstActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFirstActor();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USceneComponent> _Root;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMeshComponent> _Mesh;
+};
